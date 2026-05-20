8cca3d085ba5b28de1319b38ddcd22eada4db844
ricardo02325 <rgregorio0.@ucol.mx>
2026-05-19 19:02:37 -0600

    Más compilaciones

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,27 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
+                // App principal
                 'resources/css/app.css',
                 'resources/js/app.js',
 
+                // Admin
+                'resources/css/admin/app.css',
+                'resources/js/admin/app.js',
+
+                // Dashboard
                 'resources/css/admin/dashboard.css',
                 'resources/js/admin/dashboard.js',
+
+                // Médicos
+                'resources/css/admin/medicos.css',
+                'resources/js/admin/medicos.js',
+
+                // Pacientes
+                'resources/css/admin/pacientes.css',
+                'resources/js/admin/pacientes.js',
             ],
+
             refresh: true,
         }),
 
